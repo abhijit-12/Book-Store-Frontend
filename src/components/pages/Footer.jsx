a44e56390c195c57bf6a406ca0744a9db519343e
Abhijit das <adas0861@gmail.com>
2024-07-19 15:56:33 +0530

    footer change 2

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,26 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    
-   <>
-   <div>
+    <>
+      <div>
+        <footer className="bg-body-tertiary text-center">
+          <div className="container p-4"></div>
 
-   <footer className="bg-body-tertiary text-center">
- 
-  <div className="container p-4"></div>
-  
-
-  
-  <div className="text-center p-3">
-    © 2024 Copyright Abhijit Das:
-    <a className="text-body" href="#"></a>
-  </div>
-
-</footer>
-   </div>
+          <div className="text-center p-3">© 2024 Copyright Abhijit Das:</div>
+        </footer>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
